246c0e13107a2f7e3442b47e2d2b93a7276a3d95
AZafirovski <andrej.zafirovski@students.finki.ukim.mk>
2024-09-17 07:49:41 +0200

    Add a translation file for the contact module

--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -3,15 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactPageComponent } from './containers/contact-page/contact-page.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/contact/', '.json');
+}
 
 @NgModule({
-  declarations: [
-    ContactPageComponent
-  ],
+  declarations: [ContactPageComponent],
   imports: [
     CommonModule,
-    ContactRoutingModule
-  ]
+    ContactRoutingModule,
+    HttpClientModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'en',
+      isolate: true,
+    }),
+  ],
 })
-export class ContactModule { }
+export class ContactModule {}
