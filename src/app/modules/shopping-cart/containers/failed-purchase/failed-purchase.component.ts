6f58887681cd8dd300602a79c2855d862fbbee96
AZafirovski <andrej.zafirovski@students.finki.ukim.mk>
2024-09-23 20:54:48 +0200

    Complete the failure checkout page

--- src/app/modules/shopping-cart/containers/failed-purchase/failed-purchase.component.ts
+++ src/app/modules/shopping-cart/containers/failed-purchase/failed-purchase.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-failed-purchase',
   templateUrl: './failed-purchase.component.html',
-  styleUrls: ['./failed-purchase.component.scss']
+  styleUrls: ['./failed-purchase.component.scss'],
 })
 export class FailedPurchaseComponent {
+  constructor(private router: Router) {}
 
+  returnBack(): void {
+    history.back();
+  }
 }
